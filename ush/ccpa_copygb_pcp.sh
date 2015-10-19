dd6ff2a16c6b00d826b650b0c1d46adcd888358b
yan.luo@noaa.gov <yan.luo@noaa.gov>
2015-10-19 18:27:27 +0000

    upgrade ccpa_2015 branch: add ecf and versions directories and change script, sorc and fix files

--- ush/ccpa_copygb_pcp.sh
+++ ush/ccpa_copygb_pcp.sh
@@ -4,6 +4,7 @@
 #HISTORY:
 #2008:       Scripts available through the work of Mike Charles
 #03/24/2010: Script slightly modified by Dingchen Hou for CCPA initial implementation
+#06/30/2015: Script slightly modified by Yan Luo for adding 2.5KM NDGD grid
 ############################################################
 
 set -x 
@@ -22,16 +23,15 @@ res=$2
 filename=$3
 
 # Settings
-Copygb=/nwprod/util/exec/copygb
 OUT='.'
 
 # Set grid specifications
 case $res in
 
- 1.0d) grid="3";;
- 0.5d) grid="255 0 720 361 -90000 000000 128 90000 359500 500 500 64";;
- 0.25d) grid="255 0 232 112 25125 235125 128 52875 292875 250 250 64";;
- 0.125d) grid="110 0 464 224 250625 2350625 128 529375 2929375 125 125 64";;
+ 1p0) grid="3";;
+ 0p5) grid="255 0 720 361 -90000 000000 128 90000 359500 500 500 64";;
+ 0p25) grid="255 0 232 112 25125 235125 128 52875 292875 250 250 64";;
+ 0p125) grid="110 0 464 224 250625 2350625 128 529375 2929375 125 125 64";;
  8thd) grid="255 0 464 224 25125 235125 128 53000 293000 125 125 64";;
  110) grid="110 0 464 224 250625 2350625 128 529375 2929375 125 125 64";;
  cpc_8thd) grid="255 0 601 241 20000 230000 128 50000 305000 125 125 64";;
@@ -44,8 +44,9 @@ case $res in
 
  197) grid="197 3 1073 689 20192 238446 8 265000 5079 5079 0 64 25000 25000 0 0 0";;
  ndfd) grid="197 3 1073 689 20192 238446 8 265000 5079 5079 0 64 25000 25000 0 0 0";;
- ndgd) grid="255 3 1073 689 20192 238446 8 265000 5079 5079 0 64 25000 25000 0 0 0";;
+ ndgd5p0) grid="255 3 1073 689 20192 238446 8 265000 5079 5079 0 64 25000 25000 0 0 0";;
  rtma) grid="255 3 1073 689 20191 238445 8 265000 5079 5079 0 64 25000 25000 0 0 0";;
+ ndgd2p5) grid="255 3 2145 1377 20192 238446 8 265000 2539 2539 0 64 25000 25000 -8364400 0 0";;
 
 esac
 
@@ -71,7 +72,7 @@ set -x
 infile=$filename.grb
 outfile=${filename}_${res}.grb
 echo Interpolating $infile to $outfile
-$Copygb -N namelist.temp -i"$interp" -g"$grid" -x $infile $OUT/$outfile 
+$COPYGB -N namelist.temp -i"$interp" -g"$grid" -x $infile $OUT/$outfile 
 if [ -s ${filename}_${res}.grb ]; then
   echo Interpolation Done!!!
 else
